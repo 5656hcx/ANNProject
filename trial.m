6d83ed85f655bb8b8cf1fc4f5661e696c52ec668
Chenxi Han <5656hcx@gmail.com>
2018-08-01 21:14:06 +0800

    Added training detail indicator

--- trial.m
+++ trial.m
@@ -12,7 +12,7 @@ x = 0:2/(N-1):2;
 
 learning_rate = 0.01;
 num_iteration = 1000;
-err_precision = 1e-1;
+err_precision = 1e-3;
 
 w = randn(H,1);
 b = zeros(H,1);
@@ -24,33 +24,39 @@ grad_v = zeros(1,H);
 
 % trainning process %
 
+tic;
 for loop=1:num_iteration
+    
     for i=1:H
-        temp_w = sym('w',[H,1]);
-        temp_b = sym('b',[H,1]);
-        temp_v = sym('v',[1,H]);
-
+        tmp_w = sym('w',[H,1]);
+        tmp_b = sym('b',[H,1]);
+        tmp_v = sym('v',[1,H]);
+        
         for j=1:H
             if j~=i
-                temp_w = subs(temp_w,temp_w(j),w(j));
-                temp_b = subs(temp_b,temp_b(j),b(j));
-                temp_v = subs(temp_v,temp_v(j),v(j));
+                tmp_w = subs(tmp_w,tmp_w(j),w(j));
+                tmp_b = subs(tmp_b,tmp_b(j),b(j));
+                tmp_v = subs(tmp_v,tmp_v(j),v(j));
             end
         end
-
-        grad_w(i) = subs(diff(costFunction(x,temp_w,b,v),temp_w(i)),temp_w(i),w(i));
-        grad_b(i) = subs(diff(costFunction(x,w,temp_b,v),temp_b(i)),temp_b(i),b(i));
-        grad_v(i) = subs(diff(costFunction(x,w,b,temp_v),temp_v(i)),temp_v(i),v(i));
+        
+        grad_w(i) = subs(diff(costFunction(x,tmp_w,b,v),tmp_w(i)),tmp_w(i),w(i));
+        grad_b(i) = subs(diff(costFunction(x,w,tmp_b,v),tmp_b(i)),tmp_b(i),b(i));
+        grad_v(i) = subs(diff(costFunction(x,w,b,tmp_v),tmp_v(i)),tmp_v(i),v(i));
+        
     end
     
-    w = w - learning_rate * grad_w;
+    w = w - learning_rate * grad_w; 
     b = b - learning_rate * grad_b; 
     v = v - learning_rate * grad_v; 
     
-    if costFunction(x,w,b,v) < err_precision
+    cost = costFunction(x,w,b,v);
+    disp(['loop: ',num2str(loop),' | ','error: ', num2str(cost)]);
+    toc;
+    
+    if cost < err_precision
         break;
     end
-    
 end
 
 %  output illustration %
@@ -60,5 +66,5 @@ predict_x = 0:2/9:2;
 
 plot(predict_x,result .* (predict_x),'r-o');
 hold on;
-plot(predict_x,analytic(predict_x), 'b-o');
+plot(predict_x,analytic(predict_x), 'b-x');
 disp(cost);
