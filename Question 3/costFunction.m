92f37685ac3d77dbb1121c549ec1d5fb815b5028
Chenxi Han <5656hcx@gmail.com>
2018-08-09 19:24:18 +0800

    Fixed an error in cost calculation

--- Question 3/costFunction.m
+++ Question 3/costFunction.m
@@ -1,15 +1,10 @@
-function [cost,grads] = costFunction(x,p,A)
+function cost = costFunction(x,p,A)
     
     N = length(x);
-    len = length(p)/3;
-    w = p(1:len,:);
-    b = p(len+1:2*len,:);
-    v = p(2*len+1:3*len,:)';
-    
-    
-    grad_w = zeros(size(w));
-    grad_b = zeros(size(b));
-    grad_v = zeros(size(v));
+    H = length(p)/3;
+    w = p(1:H,:);
+    b = p(H+1:2*H,:);
+    v = p(2*H+1:3*H,:)';
     
     d_1 = diff(tSolution(sym('x'),w,b,v,A),1);
     d_2 = diff(tSolution(sym('x'),w,b,v,A),2);
@@ -18,9 +13,10 @@ function [cost,grads] = costFunction(x,p,A)
     for j=1:N
         tmp_1d = double(subs(d_1,x(j)));
         tmp_2d = double(subs(d_2,x(j)));
-        tmp_cost = tmp_2d + 1/5 * tmp_1d + tSolution(x(j),w,b,v,A) + xFunction(x(j));
+        tmp_cost = tmp_2d + 1/5 * tmp_1d + tSolution(x(j),w,b,v,A) - xFunction(x(j));
         cost = cost + tmp_cost^2;
     end
-    grads = [grad_w;grad_b;grad_v'];
+    
     toc;
+    disp(cost);
 end
--- Question 3/trial.m
+++ Question 3/trial.m
@@ -1,25 +1,29 @@
-% network parameters %
+function trial()
 
-A = [0,1];          % initial conditions
-N = 10;             % #[input values]
-H = 10;             % #[hidden nodes]
-x = 0:2/(N-1):2;    % [input vector]
+    % network parameters %
 
-init_param = [randn(H,1);zeros(H,1);randn(H,1)];
+    A = [0,1];          % initial conditions
+    N = 10;             % #[input values]
+    H = 10;             % #[hidden nodes]
+    x = 0:2/(N-1):2;    % [input vector]
 
-% trainning process %
-
-tic;
-options = optimset('Display','off','GradObj','off','HessUpdate','bfgs','MaxIter',10);
-[opt_param,cost,exit_flag] = fminunc(@(p)(costFunction(x,p,A)),init_param,options);
-
-% output illustration %
-
-w = opt_param(1:N,:);
-b = opt_param(N+1:2*N,:);
-v = opt_param(2*N+1:3*N,:)';
-
-pred_x = 0:2/19:2;
-plot(pred_x,tSolution(pred_x,w,b,v,A),'r-o');
-hold on;
-plot(pred_x,analytical(pred_x),'b-x');
+    init_param = [randn(H,1);zeros(H,1);randn(H,1)];
+    
+    % trainning process %
+    % 100 iterations would take 400 to 800 seconds %
+    
+    tic;
+    options = optimset('Display','off','HessUpdate','bfgs','MaxIter',100);
+    output = fminunc(@(p)(costFunction(x,p,A)),init_param,options);
+    
+    % output illustration %
+    
+    w = output(1:N,:);
+    b = output(N+1:2*N,:);
+    v = output(2*N+1:3*N,:)';
+    
+    pred_x = 0:2/19:2;
+    plot(pred_x,tSolution(pred_x,w,b,v,A),'r-o');
+    hold on;
+    plot(pred_x,analytical(pred_x),'b-x');
+end
