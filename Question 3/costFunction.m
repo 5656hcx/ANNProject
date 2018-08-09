37febbd42f4e59be5adfd614af715d4c8ff1c787
Chenxi Han <5656hcx@gmail.com>
2018-08-09 20:42:25 +0800

    Added training detail indicator

--- Question 3/costFunction.m
+++ Question 3/costFunction.m
@@ -9,6 +9,8 @@ function cost = costFunction(x,p,A,H)
     dx_2 = double(subs(diff(tSolution(x_,w,b,v,A),2),x));
     cost = sum((dx_2 + 1/5 * dx_1 + tSolution(x,w,b,v,A) - xFunction(x)).^2);
     
+    global eval;
+    eval = eval + 1;
+    disp(['eval: ',num2str(eval),' | ','error: ', num2str(cost)]);
     toc;
-    disp(cost);
 end
--- Question 3/trial.m
+++ Question 3/trial.m
@@ -1,7 +1,10 @@
-function trial()
-
+%function trial()
+    
+    global eval;
+    eval = 0;
+    
     % network parameters %
-
+    
     A = [0,1];          % initial conditions
     N = 10;             % #[input values]
     H = 10;             % #[hidden nodes]
@@ -10,7 +13,7 @@ function trial()
     init_param = [randn(H,1);zeros(H,1);randn(H,1)];
     
     % trainning process %
-    % 100 iterations would take 300 to 600 seconds %
+    % 100 iterations would take about 300 to 600 seconds %
     
     tic;
     options = optimset('Display','off','HessUpdate','bfgs','MaxIter',100);
@@ -26,4 +29,4 @@ function trial()
     plot(pred_x,tSolution(pred_x,w,b,v,A),'r-o');
     hold on;
     plot(pred_x,analytical(pred_x),'b-x');
-end
+%end
