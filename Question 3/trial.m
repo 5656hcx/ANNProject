6df03cbe011a7114fd34973046a34c3a9fb89fdc
Chenxi Han <5656hcx@gmail.com>
2018-08-10 09:59:50 +0800

    Fixed an error and improved the structure

--- Question 3/trial.m
+++ Question 3/trial.m
@@ -1,28 +1,20 @@
-function trial()
+function trial(x,A,H)
     
     global eval;
     eval = 0;
-    
-    % network parameters %
-    
-    A = [0,1];          % initial conditions
-    N = 10;             % #[input values]
-    H = 10;             % #[hidden nodes]
-    x = 0:2/(N-1):2;    % [input vector]
-    
-    init_param = [randn(H,1);zeros(H,1);randn(H,1)];
     
     % trainning process %
     % 100 iterations would take about 300 to 600 seconds %
     
     tic;
-    options = optimset('Display','off','HessUpdate','bfgs','MaxIter',100);
+    init_param = [randn(H,1);zeros(H,1);randn(H,1)];
+    options = optimset('Display','final','HessUpdate','bfgs','MaxIter',100);
     [output,cost] = fminunc(@(p)(costFunction(x,p,A,H)),init_param,options);
     disp(cost);
     
-    w = output(1:N,:);
-    b = output(N+1:2*N,:);
-    v = output(2*N+1:3*N,:)';
+    w = output(1:H,:);
+    b = output(H+1:2*H,:);
+    v = output(2*H+1:3*H,:)';
     
     % output illustration %
     
