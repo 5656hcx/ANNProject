cb7e119f7c2d6fa3a66203237c1f477bded644c6
Chenxi Han <5656hcx@gmail.com>
2018-08-12 20:30:50 +0800

    Improved readability and stability for Q4

--- Question_4/trial.m
+++ Question_4/trial.m
@@ -1,28 +1,42 @@
-    
+function trial()
+
     % parameters %
     
+    iter = 100;
+    tol = 1e-6;
     x = 0:3/9:3;
-    H = 10;
+    H = 3;
     P1 = [randn(H,1);zeros(H,1);randn(H,1)];
     P2 = [randn(H,1);zeros(H,1);randn(H,1)];
-    init_P = [P1;P2];
-
+    param = [P1;P2];
+    
     % trainning process %
-    % 100 iterations would take about 300 to 600 seconds %
     
     tic;
-    options = optimset('Display','final','HessUpdate','bfgs','MaxFunEval',6e+5,'MaxIter',4e+4);
-    [output,cost] = fminunc(@(p)(costFunction(x,p,H)),init_P,options);
-    disp(cost);
+    options = optimset('Display','off','HessUpdate','bfgs');
+    for i = 1:iter
+        [param,cost1] = fminunc(@(p)(costFunction(x,p,H)),param,options);
+        disp(cost1);
+        [param,cost2] = fminunc(@(p)(costFunction(x,p,H)),param,options);
+        if abs(cost2 - cost1) < tol
+            break;
+        end
+        disp(cost2);
+    end
     
-    w1 = output(1:H,:);
-    b1 = output(H+1:2*H,:);
-    v1 = output(2*H+1:3*H,:)';
-    w2 = output(3*H+1:4*H,:);
-    b2 = output(4*H+1:5*H,:);
-    v2 = output(5*H+1:6*H,:)';
+    w1 = param(1:H,:);
+    b1 = param(H+1:2*H,:);
+    v1 = param(2*H+1:3*H,:)';
+    w2 = param(3*H+1:4*H,:);
+    b2 = param(4*H+1:5*H,:);
+    v2 = param(5*H+1:6*H,:)';
     
     % output illustration %
     
-    plot(x,tSolution(x,w1,b1,v1,0)-sin(x),'g-+'); hold on;
-    plot(x,tSolution(x,w2,b2,v2,1)-(1+x.^2),'g--'); hold off;
+    hold on;
+    plot(x,tSolution(x,w1,b1,v1,0),'r-o');
+    plot(x,sin(x),'r-x');
+    plot(x,tSolution(x,w2,b2,v2,1),'b-o');
+    plot(x,1+x.^2,'b-x');
+    hold off;
+end
